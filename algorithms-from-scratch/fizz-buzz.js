150d84cb15eb256f7e5c1748e4705f84d060e945
Adrian Rodriguez <Rodriguez.Adrian609@gmail.com>
2021-02-01 14:28:44 -0500

    adding FizzBuzz practice

---
+++ algorithms-from-scratch/fizz-buzz.js
@@ -0,0 +1,17 @@
+function fizzBuzz(num) {
+  for (let i = 0; i <= num; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 == 0) {
+      console.log("Fizz");
+    } else if (i % 5 == 0) {
+      console.log("buzz");
+    } else {
+      console.log(i);
+    }
+  }
+}
+
+fizzBuzz(20);
+
+fizzBuzz(30);

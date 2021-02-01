a8a16f2e6a80d1551bfd506c3ad2a7fd82ce8245
Adrian Rodriguez <Rodriguez.Adrian609@gmail.com>
2021-01-31 22:09:43 -0500

    adding binding example to bind-method.js

--- bind-method.js
+++ bind-method.js
@@ -15,3 +15,23 @@ const roadTrip2 = {
 const getTripDistance = roadTrip1.getDistance.bind(roadTrip1, "km");
 
 console.log(getTripDistance(" in total"));
+
+const account1 = {
+  name: "jen",
+  totalAmount: 5000,
+  deductAmount: function (amount) {
+    this.totalAmount -= amount;
+    return `${this.name}, total ammount after withdawing $${amount} is $${this.totalAmount} `;
+  },
+};
+
+const account2 = {
+  name: "James",
+  totalAmount: 8000,
+};
+
+const withdrawFromAccount = function (amount) {
+  return account1.deductAmount.bind(account2, amount);
+};
+
+console.log(withdrawFromAccount(500)());
